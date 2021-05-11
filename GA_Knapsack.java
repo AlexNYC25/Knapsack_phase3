c13ebc90300764e6ee91d7718b2acaefcd40217b
Alexis Montes <montes.alexis25@gmail.com>
2021-05-10 21:15:05 -0400

    finished initial terminal version of project

--- GA_Knapsack.java
+++ GA_Knapsack.java
@@ -1,48 +1,208 @@
 import java.lang.Math;
+import java.util.Scanner;
 import java.util.Vector;
 
 public class GA_Knapsack {
 
     private Vector<boolean[]> generationPopulation;
 
+    private int tempNum = 0;
+
     private int currentGen;
     private int maxGen;
     private int populationSize;
+    private int maxPossibleWeight;
+
+    int[] easyWeights = new int[10];
+    int[] easyValues = new int[10];
+
+    int[] mediumWeights = new int[15];
+    int[] mediumValues = new int[15];
+
+    int[] largeWeights = new int[20];
+    int[] largeValues = new int[20];
     
-    public GA_Knapsack(int data, int max_gen, int pop_size) {
+    public GA_Knapsack(int max_gen, int pop_size, int knapsack_weight, Scanner dataFile) {
 
         this.currentGen = 0;
         this.maxGen = max_gen;
         this.populationSize = pop_size;
+        this.maxPossibleWeight = knapsack_weight;
+
+        // read initial reference data from datafile
+        this.readDataFromFile(dataFile);
 
         // generates temp placeholder of populations with 10 chromosomes
-        this.generateInitialPopulation(10);
+        this.findBestCombinations(10);
+
+        this.findBestCombinations(15);
+
+        this.findBestCombinations(20);
     }
 
-    public void findBestCombinations(){
-        
-        // for each of the population sizes
-            // generate the initial population
-            // for the number of max generation
-                // find and cross the best parents for the next generation
-            // print the generation_max best member
+    public void readDataFromFile(Scanner dataFile){
+        String[] initialDataStrings = dataFile.nextLine().split(" ");
 
-        this.generateInitialPopulation(10);
-        for(int i = 0; i < this.maxGen; i++){
-            Vector<boolean[]> parentGroup = this.findParentGroup(this.generationPopulation, 10);
-            // cross the best parents
+        String[] easyWeightsString = initialDataStrings[0].split(",");
+        String[] easyValuesString = initialDataStrings[1].split(",");
+
+        for(int i = 0; i < 10; i++){
+            this.easyWeights[i] = Integer.parseInt(easyWeightsString[i]);
+            this.easyValues[i] = Integer.parseInt(easyValuesString[i]);
         }
+
+        String[] mediumWeightString = initialDataStrings[2].split(",");
+        String[] mediumValueStrings = initialDataStrings[3].split(",");
+
+        for(int i = 0; i < 15; i++){
+            this.mediumWeights[i] = Integer.parseInt( mediumWeightString[i]);
+            this.mediumValues[i] = Integer.parseInt( mediumValueStrings[i] );
+        }
+
+        String[] largeWeightString = initialDataStrings[4].split(",");
+        String[] largeValueString = initialDataStrings[5].split(",");
+
+        for(int i = 0; i < 20; i++){
+            this.largeWeights[i] = Integer.parseInt( largeWeightString[i] );
+            this.largeValues[i] = Integer.parseInt( largeValueString[i] );
+        }
+        
     }
 
-    private void populationCrossover(Vector<boolean[]> parents){
-        // find the best parents
+    public void findBestCombinations(int length_of_gene){
+        
+        // generate the initial population
+        this.generateInitialPopulation(length_of_gene);
+        // for each of the population sizes
+        for(int i = 0; i < this.maxGen; i++){
+            // find and cross the best parents for the next generation, 5 placeholder for number of potential parents to return
+            Vector<boolean[]> parentGroup = this.findParentGroup(this.generationPopulation, 5);
+            // cross the best parents
+            this.generateNewGeneration(parentGroup);
+        }
+        // TODO print the best candidate and info
+
+        System.out.println("Best solution found");
+        this.findBestCandidate(this.generationPopulation);
+        
+    }
+
+    private void findBestCandidate(Vector<boolean[]> group){
+        int bestFitness = 0;
+        boolean[] candidate = null;
+
+        for(int i = 0; i < group.size(); i++){
+            if( this.evaluateCandidateFitness(group.get(i)) > bestFitness){
+                bestFitness = this.evaluateCandidateFitness(group.get(i));
+                candidate = group.get(i);
+            }
+        }
+
+        for(int i = 0; i < candidate.length; i++){
+            System.out.print(candidate[i] + " ");
+
+        }
+        System.out.println("");
+        System.out.println("Best candidate fitness is : " + this.evaluateCandidateFitness(candidate));
+        System.out.println("Best candidate weight is : " + this.evaluateCandidateWeight(candidate));
+        
+    }
+
+    /*
+        called by findBestCombination
+
+        @description: function to generate the next generation of candidates using the group of parents deemed the most fit
+    */
+    private void generateNewGeneration(Vector<boolean[]> parents){
+        Vector<boolean[]> newGeneration = new Vector<boolean[]>();
+
+        // find the best "parent" and add it to the next generation vector as a "child"
+        // TODO: crate the new function to save the best candidate in the previous generation and move it to the next generation
+        
+        // for the remainder of the population group create and add new children
+        while(newGeneration.size() < this.populationSize){
+            boolean[] newChild = this.generateChildFromRandomParents(parents);
+
+            
+            newGeneration.add(newChild);
+        }
+
+        // save the new population as the object current population
+        this.generationPopulation = newGeneration;
 
     }
 
-    // generate initial population 
+    /*
+        called by generateNewGeneration
+
+        description: from the parents group select two random parenst cross them and return thier offspring
+    */
+    private boolean[] generateChildFromRandomParents(Vector<boolean[]> parents){
+        // gets the location of potential parents from the parent group
+        int parent1Location = (int) ((Math.random() * (parents.size() -1 - 0)) + 0 );
+        int parent2Location = (int) ((Math.random() * (parents.size() -1 - 0)) + 0 );
+
+        // ensure there is no occrance of the same parent appeating twice
+        while(parent1Location == parent2Location){
+            parent2Location = (int) ((Math.random() * (parents.size() - 0)) + 0 );
+
+        }
+
+        /*
+        while(this.tempNum < 50){
+            System.out.println("Parent 1: ");
+            for(int i = 0; i < parents.get(parent1Location).length; i++){
+                System.out.print(parents.get(parent1Location)[i] + " ");
+            }
+            System.out.println("");
+
+            System.out.println("Parent 2: ");
+            for(int i = 0; i < parents.get(parent2Location).length; i++){
+                System.out.print(parents.get(parent2Location)[i] + " ");
+            }
+            System.out.println(""); 
+
+            this.tempNum++;
+        }
+        */
+        
+
+        return this.crossParents(parents.get(parent1Location), parents.get(parent2Location));
+    }
+
+    /*
+        called from gererateChildFromRandomParents
+
+        description: takes two parents and using one point cross over create a child and return it
+    */
+    private boolean[] crossParents(boolean[] parent1, boolean[] parent2){
+        boolean[] newChild = new boolean[parent1.length];
+        int crossoverPoint = (int) ((Math.random() * (parent1.length-1 - 1)) + 1 );
+
+        for(int i = 0; i < crossoverPoint; i++){
+            newChild[i] = parent1[i];
+        }
+
+        for(int j = crossoverPoint; j < parent2.length; j++){
+            newChild[j] = parent2[j];
+        }
+
+        return newChild;
+    }
+
+    /*
+        called by findBestCombination
+
+        description: creates a random population with the same number as members as the number of population, with the lenght of the population chromosome as the numChromosomes
+    */
     private void generateInitialPopulation(int numChromosomes){
         // empty population before creating a new initial 
-        generationPopulation.clear();
+        if(generationPopulation != null){
+            generationPopulation.clear();
+        }
+
+        generationPopulation = new Vector<boolean[]>();
+        
         
         // for loop for how many in initial generation 
         for(int i = 0; i < populationSize; i++){
@@ -57,6 +217,11 @@ public class GA_Knapsack {
         
     }
 
+    /*
+        called by findBestCombination
+
+        description: returns a random boolean value using math.random
+    */
     private boolean generateRandomBoolean(){
         return (Math.round(Math.random())) == 1;
     }
@@ -133,14 +298,82 @@ public class GA_Knapsack {
         return fitnessScores;
     }
 
-    private int evaluateCandidateFitness(boolean[] canidate){
+    private int evaluateCandidateFitness(boolean[] candidate){
         int totalValue = 0;
         int totalWeight = 0;
 
         // iterate through array adding respective value and weight to local variables 
+        if( candidate.length == 10){
+            for(int i = 0; i < 10; i++){
+                if(candidate[i]){
+                    totalWeight += easyWeights[i];
+                    totalValue += easyValues[i];
+                }
+            }
+        }
+
+        if(candidate.length == 15){
+            for(int i = 0; i < 15; i++){
+                if(candidate[i]){
+                    totalWeight += mediumWeights[i];
+                    totalValue += mediumValues[i];
+                }
+            }
+
+        }
+
+        if(candidate.length == 20){
+            for(int i = 0; i < 20; i++){
+                if(candidate[i]){
+                    totalWeight += largeWeights[i];
+                    totalValue += largeValues[i];
+                }
+            }
+        }
 
         // if weight is more then the maximum allowed return 0
         // else return value
-        return 0;
+        if(totalWeight > this.maxPossibleWeight) return 0;
+
+        return totalValue;
+    }
+
+    private int evaluateCandidateWeight (boolean[] candidate){
+        int totalValue = 0;
+        int totalWeight = 0;
+
+        // iterate through array adding respective value and weight to local variables 
+        if( candidate.length == 10){
+            for(int i = 0; i < 10; i++){
+                if(candidate[i]){
+                    totalWeight += easyWeights[i];
+                    totalValue += easyValues[i];
+                }
+            }
+        }
+
+        if(candidate.length == 15){
+            for(int i = 0; i < 15; i++){
+                if(candidate[i]){
+                    totalWeight += mediumWeights[i];
+                    totalValue += mediumValues[i];
+                }
+            }
+
+        }
+
+        if(candidate.length == 20){
+            for(int i = 0; i < 20; i++){
+                if(candidate[i]){
+                    totalWeight += largeWeights[i];
+                    totalValue += largeValues[i];
+                }
+            }
+        }
+
+        // if weight is more then the maximum allowed return 0
+        // else return value
+
+        return totalWeight;
     }
 }
--- Knapsack.java
+++ Knapsack.java
@@ -1,8 +1,42 @@
-
+import java.io.File;
+import java.util.Scanner;
 
 public class Knapsack{
 
-    public Knapsack(){
+    public static int doesItInclude(String[] arr, String word){
+
+        for(int x = 0; x < arr.length; x++){
+            if(arr[x].matches(word)){
+                return x;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args){
+        Scanner sc = null;
+
+
+        // argument flags
+        int inputPosition = doesItInclude(args, "-i") + 1;
+        int outputPosition = doesItInclude(args, "-o") + 1;
+
+        if(inputPosition >= args.length || outputPosition >= args.length || inputPosition == -1 || outputPosition == -1){
+            System.out.println("There is a missing parameter");
+            return;
+        }
         
+        // initial data file
+        try{
+            File file = new File(args[inputPosition]); 
+            sc = new Scanner(file); 
+        } catch( Exception e){
+            System.out.println("Error found");
+        }
+
+        GA_Knapsack geneticKnapsack = new GA_Knapsack(10,1000, 30, sc);
+        
+
     }
 }
